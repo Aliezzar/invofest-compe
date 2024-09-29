2b21f92197235608f8aa92a4c55058ea9cf1f9a6
Aliezzar <aliezzar42@gmail.com>
2024-09-29 09:26:15 +0700

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var nav = this.document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+
 
 // untuk hamburger
 
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
-menuToggle.addEventListener('click', function() {
+menuToggle.addEventListener('click', function () {
     nav.classList.toggle('slide')
+});
+
+
+// Untuk animasi mengetik
+var typed = new Typed('.typing-text', {
+    strings: ["Masa Depan Berkelanjutan.", "Planet yang Lebih Baik."],
+    typeSpeed: 50,
+    loop: true
 });
